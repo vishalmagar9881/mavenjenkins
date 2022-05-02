8510ea7b531f860aca3628d6ea657797b478f5ea
vishalmagar9881 <102547606+vishalmagar9881@users.noreply.github.com>
2022-05-02 13:57:33 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -3,6 +3,7 @@ package mavenforjenkins;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
