c8e2af857478bd3b4e880a132cfdc5898e6b1357
vishalmagar9881 <102547606+vishalmagar9881@users.noreply.github.com>
2022-05-02 14:04:00 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
+from selenium.webdriver.chrome.options import Options;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest 
@@ -23,9 +23,12 @@ public class UITest
 		if(browserName.contains("Chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--disable-dev-shm-usage");
-			 driver=new ChromeDriver();
+			chrome_options = Options();
+			chrome_options.add_argument("--disable-extensions");
+			chrome_options.add_argument("--disable-gpu");
+			chrome_options.add_argument("--no-sandbox");
+			options.add_argument("--disable-dev-shm-usage");
+			driver=new ChromeDriver(options=chrome_options);
 		}
 		else if(browserName.contains("Edge"))
 		{
